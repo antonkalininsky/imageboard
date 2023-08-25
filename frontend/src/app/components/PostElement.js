fe2a592a78b119cc4f1e1da9c0fbca08b13c5aef
Anton <chadik.free@gmail.com>
2023-08-25 09:38:04 +0600

    updating post data

--- frontend/src/app/components/PostElement.js
+++ frontend/src/app/components/PostElement.js
@@ -1,7 +1,22 @@
 export default function PostElement({data}) {
     return (
-        <div className="m-2 p-6 bg-white max-w">
-            {data.text}
+        <div className="m-2 p-4 bg-white max-w">
+            {/* header */}
+            <div className="text-gray-500">
+                <span>{data.name}</span>
+                <span className="ml-3">{data.dateTime}</span>
+                <span className="ml-3 text-green-600">{data.number}</span>
+                <button className="bg-gray-200 p-1 ml-3">опции</button>
+            </div>
+            {/* body */}
+            <div className="ml-4">
+                {data.text}
+            </div>
+            {/* footer */}
+            <div className="ml-4">
+                {data.links}
+            </div>
+            
         </div>
     )
 }
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from 'next/image'
 import PostElement from './components/PostElement'
 import CreatePostForm from './components/CreatePostForm'
 import { useState, useEffect } from 'react'
@@ -9,24 +8,25 @@ export default function Home() {
 
   const [dataList, setDataList] = useState([
     {
-      text: 'hello 1',
+      name: 'ОП',
+      dateTime: '24.08.2023 Чт 9:28:40',
+      number: 1,
+      links: [1],
+      text: 'Сап /б/',
       id: 0
     },
     {
-      text: 'hello 2',
+      name: 'Аноним',
+      dateTime: '25.08.2023 Пт 9:28:40',
+      number: 2,
+      links: [],
+      text: 'Нет',
       id: 1
-    },
-    {
-      text: 'hello 3',
-      id: 2
-    },
-    {
-      text: 'hello 4',
-      id: 3
     },
   ])
   const [postList, setPostList] = useState([])
-  const [cou, setCou] = useState(4)
+  const [idCounter, setIdCounter] = useState(2)
+  const [number, setNumber] = useState(3)
 
   useEffect(() => {
     setPostList(dataList.map((data) => <PostElement data={data} key={data.id} />))
@@ -36,11 +36,25 @@ export default function Home() {
   function createPost(value) {
     setDataList(
       [...dataList, {
+        name: 'Аноним',
+        dateTime: getDateTime(),
+        number,
+        links: [],
         text: value,
-        id: cou
+        id: idCounter
       }]
     )
-    setCou(cou + 1)
+    setIdCounter(idCounter + 1)
+    setNumber(number + 1)
+  }
+
+  function getDateTime() {
+    const dateSrc = new Date()
+    const date = dateSrc.toLocaleDateString('ru')
+    const weekday = dateSrc.getDay()
+    const weekdays = ['Вс', 'Пн', 'Вт', 'Ср' ,'Чт' ,'Пт' ,'Сб']
+    const time = `${dateSrc.getHours()}:${dateSrc.getMinutes()}:${dateSrc.getSeconds()}`
+    return `${date} ${weekdays[weekday]} ${time}`
   }
 
   return (
